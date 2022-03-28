f648f03d5d30e3c778bafc4fa872bcb341527641
David Mendoza <hectormza21@gmail.com>
2022-03-28 14:02:21 -0400

    content distribution done

--- src/index.js
+++ src/index.js
@@ -67,4 +67,3 @@ addTaskButtonLogic(weekContent).divMakerEventListener();
 
 
 
-
--- src/todoMaker.js
+++ src/todoMaker.js
@@ -52,8 +52,10 @@ const addTaskButtonLogic = (contentInstance) => {
             //* content distribution-----------------------------
             homeContent.addContent(todoDivMaker(contentInstance).todoDiv);
     
-            //this line is confusing, need to change it, make it more readable.
-    
+            /* this line is a little confusing, so ill explain it:
+                it is the formating of the date input taken by the date input in the 'addButton' ghost div. The reason that the function todoDivMaker is called here is because it returns the newTodo due date. It is used to check if the date of the input is equal to today's date. In that case, the new todo div will be appended to the 'today' content tab. 
+            
+            */
             if (format(todoDivMaker(contentInstance).newTodo.dueDate, "MM/dd/yyyy") === today){
                 todayContent.addContent(todoDivMaker(contentInstance).todoDiv);
             };
@@ -80,75 +82,6 @@ return {divMakerEventListener}
 
 
 export {addTaskButtonLogic};
-
-
-
-//!-dont delete this, im refactoring it!-----------------
-
-/* const addTaskButtonLogic = (contentInstance) => {return null};
-const makeTodo = (contentInstance) => { 
-    let todo = {
-        divMade: false,
-        dueDate: new Date(parseISO(contentInstance.inputList[2].value)),
-        title: contentInstance.inputList[0].value,
-        description: contentInstance.inputList[1].value,
-        projectName: contentInstance.inputList[3].value,
-    };
-
-    return todo;
-};
-
-const todoDivMaker = (contentInstance) => {
-    let newTodo = makeTodo(contentInstance);
-    
-    todoDataBase.push(newTodo); //sends todo to the database.
-    
-    let todoDiv = document.createElement('li');
-
-    let titleP = document.createElement('p');
-        titleP.textContent = `${newTodo.title}`;
-        todoDiv.appendChild(titleP);
-
-    let dateP = document.createElement('p');
-        dateP.textContent = `${format(newTodo.dueDate, "MM/dd/yyyy")}`;
-        todoDiv.appendChild(dateP);
-
-return {todoDiv, newTodo};
-
-};
-
-const divMakerEventListener = (contentInstance) => {
-
-    let today = format(new Date(), "MM/dd/yyyy");
-
-    contentInstance.svgAddButton.addEventListener('click', () =>{
-
-
-        //* content distribution-----------------------------
-        homeContent.addContent(todoDivMaker(contentInstance).todoDiv);
-
-        //this line is confusing, need to change it, make it more readable.
-
-        if (format(todoDivMaker(contentInstance).newTodo.dueDate, "MM/dd/yyyy") === today){
-            todayContent.addContent(todoDivMaker(contentInstance).todoDiv);
-        };
-
-        //? note : isThisWeek() bugs out if given a formated new Date() object, so i needed to convert back the formated date into a new Date and it worked for some reason. I have no idea why. But it works.
-
-        //todo: sundays dont seem to count as 'this week', need to fix that.
-
-        if (isThisWeek(new Date(todoDivMaker(contentInstance).newTodo.dueDate)) === true) {
-            weekContent.addContent(todoDivMaker(contentInstance).todoDiv);
-        };
-        //*-----------------------------------------------------
-
-        contentInstance.ghostDiv.style.display = "none";
-        contentInstance.svgButtonsDiv.style.display = "none";
-    });
-};
-
-export {divMakerEventListener};
- */
 
 
 
