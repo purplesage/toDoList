01185dd87fd7b2c743c4f6badb89318b46a09710
David Mendoza <hectormza21@gmail.com>
2022-04-04 15:53:41 -0400

    edit button done

--- src/todoMaker.js
+++ src/todoMaker.js
@@ -61,7 +61,7 @@ const addTaskButtonLogic = (contentInstance) => {
 
             })
 
-        //todo: description button element------(currently under work)----------
+        //* description-eddit button element---------------------------
 
         let todoDescriptionSvg = document.createElement('div');
             todoDescriptionSvg.textContent = "descriptionSVG";
@@ -71,61 +71,88 @@ const addTaskButtonLogic = (contentInstance) => {
 
         
         let descriptionDiv = document.createElement('div');
-        
+        descriptionDiv.setAttribute('id', 'description-element-div');
+
         let descriptionXbutton = document.createElement('button');
         descriptionXbutton.textContent = 'X';
-        
+
+        let descriptionVbutton = document.createElement('button');
+        descriptionVbutton.textContent = 'V';
+
+        todoDescriptionSvg.addEventListener('click', () => {
+            
+            descriptionDiv.style.display = "flex";
+            descriptionDiv.classList = 'description-element-div';
+
+            for (let key in todoDivObject) {
+                let descriptionElement = document.createElement('input');
+                
+                if (key === 'title') {
+                    descriptionElement.setAttribute('type', 'text');
+                    descriptionElement.setAttribute('placeholder', `Title: ${todoDivObject.title}`);
+                    todoDivObject.title = descriptionElement.value;
+
+                }else if (key === "description") {
+                    descriptionElement.setAttribute('type', 'text');
+                    descriptionElement.setAttribute('placeholder', `Description: ${todoDivObject.description}`);
+                    todoDivObject.description = descriptionElement.value;
+
+                }else if (key === 'dueDate') {
+                    descriptionElement.setAttribute('type', 'date');
+                    descriptionElement.value = `${todoDivObject.dueDate}`
+                    todoDivObject.dueDate = descriptionElement.value;
+
+                }else if (key === "projectName") {
+                    descriptionElement.setAttribute('type', 'text');
+                    descriptionElement.setAttribute('placeholder', `Project name: ${todoDivObject.projectName}`);
+                    
+                }else{
+                    break;
+                }
+
+                descriptionDiv.appendChild(descriptionElement);
+
+            };
+
+            descriptionDiv.appendChild(descriptionXbutton);
+            descriptionDiv.appendChild(descriptionVbutton);
+            mainDiv.appendChild(descriptionDiv);  
+    });    
 
         descriptionXbutton.addEventListener('click', () => {
 
             descriptionDiv.style.display = "none";
             descriptionDiv.innerHTML = "";
             
+        }); 
+    
+        descriptionVbutton.addEventListener('click', () => {
+
+            let descriptionInputs = document.getElementById('description-element-div').getElementsByTagName('input');
+
+            todoDivObject.title = descriptionInputs[0].value;
+            todoDivObject.description = descriptionInputs[1].value;
+            todoDivObject.dueDate = new Date(parseISO(descriptionInputs[2].value));
+            todoDivObject.projectName = descriptionInputs[3].value;
+
+            todoDivObject.div.innerHTML = `<p>${todoDivObject.title}</p><p>${format(todoDivObject.dueDate, "MM/dd/yyyy")}</p>`;
+
+            todoDivObject.div.appendChild(todoDescriptionSvg);
+            todoDivObject.div.appendChild(deleteTodoSvg);
+
+            descriptionDiv.style.display = "none";
+            descriptionDiv.innerHTML = "";
+
         });
 
-
-            todoDescriptionSvg.addEventListener('click', () => {
-                
-                    descriptionDiv.style.display = "flex";
-                    descriptionDiv.classList = 'description-element-div';
-
-                    for (let key in todoDivObject) {
-                        let descriptionElement = document.createElement('input');
-                        
-                        if (key === 'title') {
-                            descriptionElement.setAttribute('type', 'text');
-                            descriptionElement.setAttribute('placeholder', `Title: ${todoDivObject.title}`);
-                            todoDivObject.title = descriptionElement.value;
-
-                        }else if (key === "description") {
-                            descriptionElement.setAttribute('type', 'text');
-                            descriptionElement.setAttribute('placeholder', `Description: ${todoDivObject.description}`);
-                            todoDivObject.description = descriptionElement.value;
-
-                        }else if (key === 'dueDate') {
-                            descriptionElement.setAttribute('type', 'date');
-                            descriptionElement.value = `${todoDivObject.dueDate}`
-                            todoDivObject.dueDate = descriptionElement.value;
-
-                        }else if (key === "projectName") {
-                            descriptionElement.setAttribute('type', 'text');
-                            descriptionElement.setAttribute('placeholder', `Project name: ${todoDivObject.projectName}`);
-                            
-                        }
-
-                        descriptionDiv.appendChild(descriptionElement);
-
-                    };
-
-                    descriptionDiv.appendChild(descriptionXbutton);
-                    mainDiv.appendChild(descriptionDiv);  
-            });
         //-----------------------------------------------
 
         todoDivObject.div = todoDiv;
 
             todoDataBase.push(todoDivObject); //sends todo to the database.
           
+            //*project filter------------------------
+
             if (todoDivObject.projectName !== "") {
 
                 projectFilter(todoDivObject)  //check projectFilter.js for more info
